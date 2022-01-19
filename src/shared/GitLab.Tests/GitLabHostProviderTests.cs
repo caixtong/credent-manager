hickford/git-credential-manager

Web browser and PAT support for GitLab.

src/shared/GitLab.Tests/GitLabHostProviderTests.cs
@@ -1,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GitCredentialManager;
+using GitCredentialManager.Authentication.OAuth;
+using GitCredentialManager.Tests.Objects;
+using Moq;
+using Xunit;
+
+namespace GitLab.Tests
+{
+    public class GitLabHostProviderTests
+    {
+        [Theory]
+        [InlineData("https", "gitlab.com", true)]
+        [InlineData("http", "gitlab.com", true)]
+        [InlineData("https", "gitlab.freedesktop.org", true)]
+        [InlineData("https", "gitlab.gnome.org", true)]
+        [InlineData("https", "github.com", false)]
+        public void GitHubHostProvider_IsSupported(string protocol, string host, bool expected)
+        {
+            var input = new InputArguments(new Dictionary<string, string>
+            {
+                ["protocol"] = protocol,
+                ["host"] = host,
+            });
+
+            var provider = new GitLabHostProvider(new TestCommandContext());
+            Assert.Equal(expected, provider.IsSupported(input));
+        }
+    }
+}

src/shared/GitLab/GitLabConstants.cs
@@ -1,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GitLab
+{
+    public record GitLabInstance
+    {
+        public GitLabInstance(string host, string oAuthClientId, string oAuthClientSecret)
+        {
+            Host = host;
+            OAuthClientId = oAuthClientId;
+            OAuthClientSecret = oAuthClientSecret;
+        }
+
+        public string Host { get; }
+
+        public string OAuthClientId { get; }
+        public string OAuthClientSecret { get; }
+    }
+
+
+    public static class GitLabConstants
+    {
+        // To add an instance, follow https://docs.gitlab.com/ee/integration/oauth_provider.html
+        // specify callback/redirect URI http://127.0.0.1/ and check 'confidential', 'expire access tokens', 'write_repository'
+        private static readonly GitLabInstance[] GitLabInstances =
+        {
+            new GitLabInstance(host: "gitlab.com",
+                // https://gitlab.com/oauth/applications/207177/edit,
+                oAuthClientId: "d8a14250a4d1beacaad67dd6fabaab1e0408b581ca73ae4a76cc7170d3f8afd1",
+                oAuthClientSecret : "58b5f5e0c99a5be9ac13f4ba15992cc72c5594386e82aecac94da964147d3151"
+            ),
+            new GitLabInstance(host: "gitlab.freedesktop.org",
+                // https://gitlab.freedesktop.org/oauth/applications/52
+                oAuthClientId: "6503d8c5a27187628440d44e0352833a2b49bce540c546c22a3378c8f5b74d45",
+                oAuthClientSecret: "2ae9343a034ff1baadaef1e7ce3197776b00746a02ddf0323bb34aca8bff6dc1")
+        };
+        public static readonly IDictionary<string, GitLabInstance> GitLabInstancesByHost = GitLabInstances.ToDictionary(x => x.Host, StringComparer.InvariantCultureIgnoreCase);
+
+        public static readonly Uri OAuthRedirectUri = new Uri("http://127.0.0.1/");
+        // https://docs.gitlab.com/ee/api/oauth2.html#authorization-code-flow
+        public static readonly Uri OAuthAuthorizationEndpointRelativeUri = new Uri("/oauth/authorize", UriKind.Relative);
+        public static readonly Uri OAuthTokenEndpointRelativeUri = new Uri("/oauth/token", UriKind.Relative);
+
+        public const AuthenticationModes DotComAuthenticationModes = AuthenticationModes.Browser | AuthenticationModes.Pat;
+
+        public static class EnvironmentVariables
+        {
+            public const string DevOAuthClientId = "GCM_DEV_GITLAB_CLIENTID";
+            public const string DevOAuthClientSecret = "GCM_DEV_GITLAB_CLIENTSECRET";
+            public const string DevOAuthRedirectUri = "GCM_DEV_GITLAB_REDIRECTURI";
+            public const string AuthenticationModes = "GCM_GITLAB_AUTHMODES";
+
+        }
+
+        public static class GitConfiguration
+        {
+            public static class Credential
+            {
+                public const string AuthenticationModes = "GitLabAuthModes";
+                public const string DevOAuthClientId = "GitLabDevClientId";
+                public const string DevOAuthClientSecret = "GitLabDevClientSecret";
+                public const string DevOAuthRedirectUri = "GitLabDevRedirectUri";
+            }
+        }
+    }
+}

src/shared/GitLab/GitLabHostProvider.cs
@@ -1,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GitCredentialManager;
+using GitCredentialManager.Authentication.OAuth;
+
+namespace GitLab
+{
+    public class GitLabHostProvider : HostProvider
+    {
+        // https://docs.gitlab.com/ee/integration/oauth_provider.html#authorized-applications
+        private static readonly string[] GitLabOAuthScopes =
+        {
+            "write_repository",
+        };
+
+        private readonly IGitLabRestApi _GitLabApi;
+        private readonly IGitLabAuthentication _GitLabAuth;
+
+        public GitLabHostProvider(ICommandContext context)
+            : this(context, new GitLabRestApi(context), new GitLabAuthentication(context)) { }
+
+        public GitLabHostProvider(ICommandContext context, IGitLabRestApi GitLabApi, IGitLabAuthentication GitLabAuth)
+            : base(context)
+        {
+            EnsureArgument.NotNull(GitLabApi, nameof(GitLabApi));
+            EnsureArgument.NotNull(GitLabAuth, nameof(GitLabAuth));
+
+            _GitLabApi = GitLabApi;
+            _GitLabAuth = GitLabAuth;
+        }
+
+        public override string Id => "GitLab";
+
+        public override string Name => "GitLab";
+
+        public override bool IsSupported(InputArguments input)
+        {
+            if (input is null)
+            {
+                return false;
+            }
+
+            // We do not support unencrypted HTTP communications to GitLab,
+            // but we report `true` here for HTTP so that we can show a helpful
+            // error message for the user in `CreateCredentialAsync`.
+            if (!StringComparer.OrdinalIgnoreCase.Equals(input.Protocol, "http") &&
+                !StringComparer.OrdinalIgnoreCase.Equals(input.Protocol, "https"))
+            {
+                return false;
+            }
+
+            // Split port number and hostname from host input argument
+            if (!input.TryGetHostAndPort(out string hostName, out _))
+            {
+                return false;
+            }
+
+            if (GitLabConstants.GitLabInstancesByHost.ContainsKey(hostName))
+            {
+                return true;
+            }
+
+            string[] domains = hostName.Split(new char[] { '.' });
+
+            // GitLab[.subdomain].domain.tld
+            if (domains.Length >= 3 &&
+                StringComparer.OrdinalIgnoreCase.Equals(domains[0], "gitlab"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool IsSupported(HttpResponseMessage response)
+        {
+            if (response == null)
+            {
+                return false;
+            }
+
+            // not always present
+            return response.Headers.Contains("X-Gitlab-Feature-Category");
+        }
+
+        public override string GetServiceName(InputArguments input)
+        {
+            var baseUri = new Uri(base.GetServiceName(input));
+
+            string url = baseUri.AbsoluteUri;
+
+            // Trim trailing slash
+            return url.TrimEnd('/');
+        }
+
+        public override async Task<ICredential> GenerateCredentialAsync(InputArguments input)
+        {
+            ThrowIfDisposed();
+
+            // We should not allow unencrypted communication and should inform the user
+            if (StringComparer.OrdinalIgnoreCase.Equals(input.Protocol, "http"))
+            {
+                throw new Exception("Unencrypted HTTP is not supported for GitLab. Ensure the repository remote URL is using HTTPS.");
+            }
+
+            Uri remoteUri = input.GetRemoteUri();
+
+            AuthenticationModes authModes = GetSupportedAuthenticationModes(remoteUri);
+
+            AuthenticationPromptResult promptResult = _GitLabAuth.GetAuthentication(remoteUri, input.UserName, authModes);
+
+            switch (promptResult.AuthenticationMode)
+            {
+                case AuthenticationModes.Basic:
+                    return promptResult.Credential;
+
+                case AuthenticationModes.Browser:
+                    return await GenerateOAuthCredentialAsync(remoteUri);
+
+                case AuthenticationModes.Pat:
+                    string token = promptResult.Credential.Password;
+
+                    // GitLab accepts any username https://gitlab.com/gitlab-org/gitlab/-/issues/212953
+                    string userName = promptResult.Credential.Account ?? "pat";
+
+                    return new GitCredential(userName, token);
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(promptResult));
+            }
+        }
+
+        internal AuthenticationModes GetSupportedAuthenticationModes(Uri targetUri)
+        {
+            // Check for an explicit override for supported authentication modes
+            if (Context.Settings.TryGetSetting(
+                GitLabConstants.EnvironmentVariables.AuthenticationModes,
+                Constants.GitConfiguration.Credential.SectionName, GitLabConstants.GitConfiguration.Credential.AuthenticationModes,
+                out string authModesStr))
+            {
+                if (Enum.TryParse(authModesStr, true, out AuthenticationModes authModes) && authModes != AuthenticationModes.None)
+                {
+                    Context.Trace.WriteLine($"Supported authentication modes override present: {authModes}");
+                    return authModes;
+                }
+                else
+                {
+                    Context.Trace.WriteLine($"Invalid value for supported authentication modes override setting: '{authModesStr}'");
+                }
+            }
+
+            AuthenticationModes modes = AuthenticationModes.Basic | AuthenticationModes.Pat;
+
+            try
+            {
+                GitLabOAuth2Client.GetClientId(Context.Settings, targetUri);
+            }
+            catch (Exception e)
+            {
+                Context.Streams.Error.WriteLine(e.Message);
+                return modes;
+            }
+            modes |= AuthenticationModes.Browser;
+            return modes;
+        }
+
+        private async Task<GitCredential> GenerateOAuthCredentialAsync(Uri targetUri)
+        {
+            OAuth2TokenResult result = await _GitLabAuth.GetOAuthTokenViaBrowserAsync(targetUri, GitLabOAuthScopes);
+
+            // username oauth2 https://gitlab.com/gitlab-org/gitlab/-/issues/349461
+            return new GitCredential("oauth2", result.AccessToken);
+        }
+
+        protected override void ReleaseManagedResources()
+        {
+            _GitLabApi.Dispose();
+            _GitLabAuth.Dispose();
+            base.ReleaseManagedResources();
+        }
+    }
+}

caixtong/credent-manager

src/shared/GitLab.Tests/GitLabHostProviderTests.cs
@@ -1,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GitCredentialManager;
+using GitCredentialManager.Authentication.OAuth;
+using GitCredentialManager.Tests.Objects;
+using Moq;
+using Xunit;
+
+namespace GitLab.Tests
+{
+    public class GitLabHostProviderTests
+    {
+        [Theory]
+        [InlineData("https", "gitlab.com", true)]
+        [InlineData("http", "gitlab.com", true)]
+        [InlineData("https", "gitlab.freedesktop.org", true)]
+        [InlineData("https", "gitlab.gnome.org", true)]
+        [InlineData("https", "github.com", false)]
+        public void GitLabHostProvider_IsSupported(string protocol, string host, bool expected)
+        {
+            var input = new InputArguments(new Dictionary<string, string>
+            {
+                ["protocol"] = protocol,
+                ["host"] = host,
+            });
+
+            var provider = new GitLabHostProvider(new TestCommandContext());
+            Assert.Equal(expected, provider.IsSupported(input));
+        }
+    }
+}

src/shared/GitLab/GitLabConstants.cs
@@ -1,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GitLab
+{
+    public record GitLabApplication
+    {
+        public GitLabApplication(string host, string oAuthClientId, string oAuthClientSecret)
+        {
+            Host = host;
+            OAuthClientId = oAuthClientId;
+            OAuthClientSecret = oAuthClientSecret;
+        }
+
+        public string Host { get; }
+        public string OAuthClientId { get; }
+        public string OAuthClientSecret { get; }
+    }
+
+
+    public static class GitLabConstants
+    {
+        // To add an instance, follow https://docs.gitlab.com/ee/integration/oauth_provider.html
+        // specify callback/redirect URI http://127.0.0.1/ and check 'confidential', 'expire access tokens', 'write_repository'
+        private static readonly GitLabApplication[] GitLabApplications =
+        {
+            new GitLabApplication(host: "gitlab.com",
+                // https://gitlab.com/oauth/applications/207177/edit owned by hickford
+                oAuthClientId: "d8a14250a4d1beacaad67dd6fabaab1e0408b581ca73ae4a76cc7170d3f8afd1",
+                oAuthClientSecret : "58b5f5e0c99a5be9ac13f4ba15992cc72c5594386e82aecac94da964147d3151"
+            ),
+            new GitLabApplication(host: "gitlab.freedesktop.org",
+                // https://gitlab.freedesktop.org/oauth/applications/52 owned by hickford
+                oAuthClientId: "6503d8c5a27187628440d44e0352833a2b49bce540c546c22a3378c8f5b74d45",
+                oAuthClientSecret: "2ae9343a034ff1baadaef1e7ce3197776b00746a02ddf0323bb34aca8bff6dc1")
+        };
+        public static readonly IDictionary<string, GitLabApplication> GitLabApplicationsByHost = GitLabApplications.ToDictionary(x => x.Host, StringComparer.InvariantCultureIgnoreCase);
+
+        public static readonly Uri OAuthRedirectUri = new Uri("http://127.0.0.1/");
+        // https://docs.gitlab.com/ee/api/oauth2.html#authorization-code-flow
+        public static readonly Uri OAuthAuthorizationEndpointRelativeUri = new Uri("/oauth/authorize", UriKind.Relative);
+        public static readonly Uri OAuthTokenEndpointRelativeUri = new Uri("/oauth/token", UriKind.Relative);
+
+        public const AuthenticationModes DotComAuthenticationModes = AuthenticationModes.Browser | AuthenticationModes.Pat;
+
+        public static class EnvironmentVariables
+        {
+            public const string DevOAuthClientId = "GCM_DEV_GITLAB_CLIENTID";
+            public const string DevOAuthClientSecret = "GCM_DEV_GITLAB_CLIENTSECRET";
+            public const string DevOAuthRedirectUri = "GCM_DEV_GITLAB_REDIRECTURI";
+            public const string AuthenticationModes = "GCM_GITLAB_AUTHMODES";
+
+        }
+
+        public static class GitConfiguration
+        {
+            public static class Credential
+            {
+                public const string AuthenticationModes = "GitLabAuthModes";
+                public const string DevOAuthClientId = "GitLabDevClientId";
+                public const string DevOAuthClientSecret = "GitLabDevClientSecret";
+                public const string DevOAuthRedirectUri = "GitLabDevRedirectUri";
+            }
+        }
+    }
+}

src/shared/GitLab/GitLabHostProvider.cs
@@ -1,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GitCredentialManager;
+using GitCredentialManager.Authentication.OAuth;
+
+namespace GitLab
+{
+    public class GitLabHostProvider : HostProvider
+    {
+        // https://docs.gitlab.com/ee/integration/oauth_provider.html#authorized-applications
+        private static readonly string[] GitLabOAuthScopes =
+        {
+            "write_repository",
+        };
+
+        private readonly IGitLabAuthentication _GitLabAuth;
+
+        public GitLabHostProvider(ICommandContext context)
+            : this(context, new GitLabAuthentication(context)) { }
+
+        public GitLabHostProvider(ICommandContext context, IGitLabAuthentication GitLabAuth)
+            : base(context)
+        {
+            EnsureArgument.NotNull(GitLabAuth, nameof(GitLabAuth));
+
+            _GitLabAuth = GitLabAuth;
+        }
+
+        public override string Id => "GitLab";
+
+        public override string Name => "GitLab";
+
+        public override bool IsSupported(InputArguments input)
+        {
+            if (input is null)
+            {
+                return false;
+            }
+
+            // We do not support unencrypted HTTP communications to GitLab,
+            // but we report `true` here for HTTP so that we can show a helpful
+            // error message for the user in `CreateCredentialAsync`.
+            if (!StringComparer.OrdinalIgnoreCase.Equals(input.Protocol, "http") &&
+                !StringComparer.OrdinalIgnoreCase.Equals(input.Protocol, "https"))
+            {
+                return false;
+            }
+
+            // Split port number and hostname from host input argument
+            if (!input.TryGetHostAndPort(out string hostName, out _))
+            {
+                return false;
+            }
+
+            if (GitLabConstants.GitLabApplicationsByHost.ContainsKey(hostName))
+            {
+                return true;
+            }
+
+            string[] domains = hostName.Split(new char[] { '.' });
+
+            // GitLab[.subdomain].domain.tld
+            if (domains.Length >= 3 &&
+                StringComparer.OrdinalIgnoreCase.Equals(domains[0], "gitlab"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool IsSupported(HttpResponseMessage response)
+        {
+            if (response == null)
+            {
+                return false;
+            }
+
+            // not always present https://gitlab.com/gitlab-org/gitlab/-/issues/349464
+            return response.Headers.Contains("X-Gitlab-Feature-Category");
+        }
+
+        public override string GetServiceName(InputArguments input)
+        {
+            var baseUri = new Uri(base.GetServiceName(input));
+
+            string url = baseUri.AbsoluteUri;
+
+            // Trim trailing slash
+            return url.TrimEnd('/');
+        }
+
+        public override async Task<ICredential> GenerateCredentialAsync(InputArguments input)
+        {
+            ThrowIfDisposed();
+
+            // We should not allow unencrypted communication and should inform the user
+            if (StringComparer.OrdinalIgnoreCase.Equals(input.Protocol, "http"))
+            {
+                throw new Exception("Unencrypted HTTP is not supported for GitLab. Ensure the repository remote URL is using HTTPS.");
+            }
+
+            Uri remoteUri = input.GetRemoteUri();
+
+            AuthenticationModes authModes = GetSupportedAuthenticationModes(remoteUri);
+
+            AuthenticationPromptResult promptResult = _GitLabAuth.GetAuthentication(remoteUri, input.UserName, authModes);
+
+            switch (promptResult.AuthenticationMode)
+            {
+                case AuthenticationModes.Basic:
+                    return promptResult.Credential;
+
+                case AuthenticationModes.Browser:
+                    return await GenerateOAuthCredentialAsync(remoteUri);
+
+                case AuthenticationModes.Pat:
+                    string token = promptResult.Credential.Password;
+
+                    // GitLab accepts any username https://gitlab.com/gitlab-org/gitlab/-/issues/212953
+                    string userName = promptResult.Credential.Account ?? "pat";
+
+                    return new GitCredential(userName, token);
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(promptResult));
+            }
+        }
+
+        internal AuthenticationModes GetSupportedAuthenticationModes(Uri targetUri)
+        {
+            // Check for an explicit override for supported authentication modes
+            if (Context.Settings.TryGetSetting(
+                GitLabConstants.EnvironmentVariables.AuthenticationModes,
+                Constants.GitConfiguration.Credential.SectionName, GitLabConstants.GitConfiguration.Credential.AuthenticationModes,
+                out string authModesStr))
+            {
+                if (Enum.TryParse(authModesStr, true, out AuthenticationModes authModes) && authModes != AuthenticationModes.None)
+                {
+                    Context.Trace.WriteLine($"Supported authentication modes override present: {authModes}");
+                    return authModes;
+                }
+                else
+                {
+                    Context.Trace.WriteLine($"Invalid value for supported authentication modes override setting: '{authModesStr}'");
+                }
+            }
+
+            AuthenticationModes modes = AuthenticationModes.Basic | AuthenticationModes.Pat;
+
+            try
+            {
+                GitLabOAuth2Client.GetClientId(Context.Settings, targetUri);
+            }
+            catch (Exception e)
+            {
+                Context.Streams.Error.WriteLine(e.Message);
+                return modes;
+            }
+            modes |= AuthenticationModes.Browser;
+            return modes;
+        }
+
+        private async Task<GitCredential> GenerateOAuthCredentialAsync(Uri targetUri)
+        {
+            OAuth2TokenResult result = await _GitLabAuth.GetOAuthTokenViaBrowserAsync(targetUri, GitLabOAuthScopes);
+
+            // username oauth2 https://gitlab.com/gitlab-org/gitlab/-/issues/349461
+            return new GitCredential("oauth2", result.AccessToken);
+        }
+
+        protected override void ReleaseManagedResources()
+        {
+            _GitLabAuth.Dispose();
+            base.ReleaseManagedResources();
+        }
+    }
+}